28beb535964d8b7983c00a9ef97458287d4cf0bc
joshpseb <144298690+joshpseb@users.noreply.github.com>
2024-11-17 09:49:39 -0600

    import buton

---
+++ frontend/react-vite/src/App.jsx
@@ -0,0 +1,118 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="Desktop3" style={{width: 1440, height: 1024, position: 'relative', background: 'white'}}>
+  <div className="Line1" style={{width: 881, height: 0, left: 559, top: 143, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '1px #CFCFCF solid'}}></div>
+  <div className="Line2" style={{width: 1440, height: 0, left: 0, top: 143, position: 'absolute', border: '1px #CFCFCF solid'}}></div>
+  <img className="Image1" style={{width: 324, height: 74, left: 21, top: 40, position: 'absolute'}} src="https://via.placeholder.com/324x74" />
+  <div className="Vs" style={{left: 258, top: 566, position: 'absolute', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>vs</div>
+  <div className="Rectangle4" style={{width: 1440, height: 75, left: 0, top: 143, position: 'absolute', background: '#DD4141'}} />
+  <div className="Line01" style={{width: 820, height: 526, left: 590, top: 275, position: 'absolute'}}>
+    <div className="Vector" style={{width: 820, height: 526, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 1, left: 98.60, top: 467.65, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) dotted'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 0, left: 98.60, top: 365.73, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) dotted'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 0, left: 98.60, top: 263.82, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) dotted'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 0, left: 98.60, top: 161.91, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) dotted'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 0, left: 98.60, top: 60, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) dotted'}}></div>
+    <div className="Vector" style={{width: 0, height: 407.65, left: 99.42, top: 59.17, position: 'absolute', border: '1px #A1A9BC solid'}}></div>
+    <div className="Vector" style={{width: 662.24, height: 0, left: 98.60, top: 467.65, position: 'absolute', border: '1px #A1A9BC solid'}}></div>
+    <div className="Vector" style={{width: 8.22, height: 0, left: 90.38, top: 467.65, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 8.22, height: 0, left: 90.38, top: 365.73, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 8.22, height: 0, left: 90.38, top: 263.82, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 8.22, height: 0, left: 90.38, top: 161.91, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 8.22, height: 0, left: 90.38, top: 60, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 99.42, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 143.79, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 186.51, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 230.88, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 275.25, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 319.62, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 363.99, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 408.36, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 452.73, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 497.09, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 539.82, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 584.19, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 628.56, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 672.93, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 717.29, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div className="Vector" style={{width: 0, height: 8.22, left: 761.66, top: 466.83, position: 'absolute', border: '1px rgba(130, 136, 152, 0.50) solid'}}></div>
+    <div style={{width: 11.50, height: 27.94, left: 73.95, top: 456.28, position: 'absolute', textAlign: 'right', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>0</div>
+    <div style={{width: 23.01, height: 27.94, left: 62.44, top: 354.37, position: 'absolute', textAlign: 'right', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>50</div>
+    <div style={{width: 31.22, height: 27.94, left: 54.23, top: 252.45, position: 'absolute', textAlign: 'right', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>100</div>
+    <div style={{width: 31.22, height: 27.94, left: 54.23, top: 150.54, position: 'absolute', textAlign: 'right', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>150</div>
+    <div style={{width: 34.51, height: 27.94, left: 50.94, top: 48.63, position: 'absolute', textAlign: 'right', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>200</div>
+    <div style={{width: 19.72, height: 27.94, left: 99.77, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>01</div>
+    <div style={{width: 23.01, height: 27.94, left: 142.28, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>03</div>
+    <div style={{width: 23.01, height: 27.94, left: 186.43, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>05</div>
+    <div style={{width: 21.36, height: 27.94, left: 231.40, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>07</div>
+    <div style={{width: 23.01, height: 27.94, left: 274.73, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>09</div>
+    <div style={{width: 18.08, height: 27.94, left: 321.34, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>11</div>
+    <div style={{width: 19.72, height: 27.94, left: 364.67, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>13</div>
+    <div style={{width: 19.72, height: 27.94, left: 408.82, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>15</div>
+    <div style={{width: 19.72, height: 27.94, left: 452.97, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>17</div>
+    <div style={{width: 19.72, height: 27.94, left: 497.12, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>19</div>
+    <div style={{width: 19.72, height: 27.94, left: 541.27, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>21</div>
+    <div style={{width: 23.01, height: 27.94, left: 583.78, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>23</div>
+    <div style={{width: 23.01, height: 27.94, left: 627.93, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>25</div>
+    <div style={{width: 21.36, height: 27.94, left: 672.90, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>27</div>
+    <div style={{width: 23.01, height: 27.94, left: 716.23, top: 478.47, position: 'absolute', textAlign: 'center', color: '#828898', fontSize: 11, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16.50, wordWrap: 'break-word'}}>29</div>
+    <div className="ClipPathGroup" style={{width: 668.82, height: 412.58, left: 95.31, top: 56.71, position: 'absolute'}}>
+      <div className="Zr137C0" style={{width: 668.82, height: 412.58, left: 0, top: 0, position: 'absolute'}}>
+        <div className="Vector" style={{width: 668.82, height: 412.58, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
+        <div className="Line3" style={{width: 662, height: 0, left: 3.69, top: 276.25, position: 'absolute', border: '2px #EEA750 solid'}}></div>
+      </div>
+      <div className="Group" style={{width: 663, height: 340.39, left: 3.69, top: 2.47, position: 'absolute'}}>
+        <div className="Line3" style={{width: 662, height: 0, left: 1, top: 72.79, position: 'absolute', border: '2px #50A56F dotted'}}></div>
+        <div className="Line3" style={{width: 662, height: 0, left: 0, top: 170.82, position: 'absolute', border: '2px #5078EE dotted'}}></div>
+        <div className="Line4" style={{width: 662, height: 0, left: 0, top: 271.79, position: 'absolute', border: '2px #EEA750 dotted'}}></div>
+        <div className="Vector" style={{width: 640.17, height: 340.39, left: 10.63, top: 0, position: 'absolute', border: '3px #D05759 solid'}}></div>
+      </div>
+    </div>
+    <div className="FuelEconomyMpg" style={{width: 327.74, height: 52, left: 10, top: 426.74, position: 'absolute', transform: 'rotate(-89.72deg)', transformOrigin: '0 0', textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Fuel Economy (mpg)</div>
+  </div>
+  <div className="Component1" style={{width: 336, height: 96, left: 108, top: 491, position: 'absolute'}}>
+    <div className="Rectangle5" style={{width: 336, height: 96, left: 0, top: 0, position: 'absolute', background: '#DD4141', borderRadius: 10}} />
+    <div className="ImportExcelFile" style={{width: 243, height: 59, left: 46, top: 28, position: 'absolute', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Import Excel File</div>
+    <div className="ImportExcelFile" style={{width: 243, height: 59, left: 46, top: 28, position: 'absolute', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Import Excel File</div>
+  </div>
+  <div className="MeasuredDataVsFuelEconomy" style={{width: 647, height: 38, left: 396, top: 162, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Measured Data vs. Fuel Economy</div>
+  <div className="Component2" style={{width: 336, height: 52, left: 875, top: 801, position: 'absolute'}}>
+    <div className="Rectangle5" style={{width: 336, height: 52, left: 0, top: 0, position: 'absolute', background: '#C3C4C7', borderRadius: 10}} />
+    <div className="Temperature" style={{width: 243, height: 31.96, left: 46, top: 10, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Temperature</div>
+    <div className="ArrowDownContained01" style={{width: 24, height: 24, left: 289, top: 15, position: 'absolute'}}>
+      <div className="Icon" style={{width: 18, height: 18, left: 3, top: 3, position: 'absolute', border: '2px white solid'}}></div>
+    </div>
+  </div>
+  <div className="Component3" style={{width: 336, height: 52, left: 876, top: 872, position: 'absolute'}}>
+    <div className="Rectangle5" style={{width: 336, height: 52, left: 0, top: 0, position: 'absolute', background: '#C3C4C7', borderRadius: 10}} />
+    <div className="Corolla" style={{width: 256, height: 32, left: 33, top: 10, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Corolla</div>
+    <div className="ArrowDownContained01" style={{width: 24, height: 24, left: 289, top: 15, position: 'absolute'}}>
+      <div className="Icon" style={{width: 18, height: 18, left: 3, top: 3, position: 'absolute', border: '2px white solid'}}></div>
+    </div>
+  </div>
+  <div className="Component4" style={{width: 352, height: 27, left: 1026, top: 233, position: 'absolute', justifyContent: 'center', alignItems: 'center', gap: 5, display: 'inline-flex'}}>
+    <div className="Rectangle6" style={{width: 14, height: 14, background: '#5078EE'}} />
+    <div className="AverageMpgForSelectedModel" style={{width: 333, height: 27, textAlign: 'center', color: '#C3C4C7', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Average mpg for selected model</div>
+  </div>
+  <div className="Component5" style={{width: 352, height: 27, left: 1026, top: 287, position: 'absolute', justifyContent: 'center', alignItems: 'center', gap: 5, display: 'inline-flex'}}>
+    <div className="Rectangle6" style={{width: 14, height: 14, background: '#EEA750'}} />
+    <div className="AverageMpgForSelectedModel" style={{width: 333, height: 27, textAlign: 'center', color: '#C3C4C7', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>City mpg for selected model</div>
+  </div>
+  <div className="Component6" style={{width: 352, height: 27, left: 1026, top: 260, position: 'absolute', justifyContent: 'center', alignItems: 'center', gap: 5, display: 'inline-flex'}}>
+    <div className="Rectangle6" style={{width: 14, height: 14, background: '#50A56F'}} />
+    <div className="AverageMpgForSelectedModel" style={{width: 333, height: 27, textAlign: 'center', color: '#C3C4C7', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}> Highway mpg for selected model</div>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
